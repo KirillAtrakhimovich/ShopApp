8dd2ae3e03880a279f066e928e900c1936d384c9
Kirill <kirill.atrakhimovich@gmail.com>
2022-12-22 14:28:59 +0300

    home refactoring

--- Shop/Home/HomeView.swift
+++ Shop/Home/HomeView.swift
@@ -5,4 +5,8 @@
 //  Created by Kirill Atrakhimovich on 22.12.22.
 //
 
-import Foundation
+import UIKit
+
+class HomeView: UIView {
+    
+}
--- Shop/Home/HomeViewController.swift
+++ Shop/Home/HomeViewController.swift
@@ -5,4 +5,18 @@
 //  Created by Kirill Atrakhimovich on 22.12.22.
 //
 
-import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    private let homeView = HomeView()
+    
+    override func loadView() {
+        view = homeView
+    }
+    
+    override func viewDidLoad() {
+        homeView.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+    }
+    
+}
