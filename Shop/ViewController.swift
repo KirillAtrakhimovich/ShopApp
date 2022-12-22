67b4ee1508b33eec4ffa73681deeedc97b8ece68
Kirill <kirill.atrakhimovich@gmail.com>
2022-12-22 11:06:13 +0300

    Initial Commit

---
+++ Shop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shop
+//
+//  Created by Kirill Atrakhimovich on 22.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
