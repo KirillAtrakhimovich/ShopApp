12990189d7363a774cc56f3868121cf755151d23
Kirill <kirill.atrakhimovich@gmail.com>
2023-01-10 21:48:30 +0300

    coding

--- Shop/Home/HomeViewController.swift
+++ Shop/Home/HomeViewController.swift
@@ -17,6 +17,7 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         homeView.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+        homeView.setup()
     }
     
 }
