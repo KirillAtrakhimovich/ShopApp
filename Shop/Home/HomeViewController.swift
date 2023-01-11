ceece5aa48d8e926e2e3e7a4510570e2902d9bf5
Kirill <kirill.atrakhimovich@gmail.com>
2023-01-11 22:20:14 +0300

    refactoring

--- Shop/Home/HomeViewController.swift
+++ Shop/Home/HomeViewController.swift
@@ -18,6 +18,29 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         homeView.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
         homeView.setup()
+        setupCollectionSettings()
     }
+    
+    private func setupCollectionSettings() {
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewFlowLayout.init())
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.itemSize = CGSize(width: 100, height: 100)
+        
+    }
+}
+
+extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath)
+                myCell.backgroundColor = UIColor.blue
+                return myCell
+    }
+    
     
 }
