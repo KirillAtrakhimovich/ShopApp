fde05c033e48b619ed230a973572279bc7b8127d
Kirill <kirill.atrakhimovich@gmail.com>
2023-01-13 22:09:26 +0300

    coding

--- Shop/Home/HomeView.swift
+++ Shop/Home/HomeView.swift
@@ -25,23 +25,50 @@ class HomeView: UIView {
         return locationLabel
     }()
     
+    var pointImage: UIImageView = {
+        let pointImage = UIImageView()
+        pointImage.image = UIImage(named: "point_image")
+        return pointImage
+    }()
+    
     var locationImage: UIImageView = {
         let locationImage = UIImageView()
-        locationImage.image = UIImage(systemName: "mappin.and.ellipse")
-        locationImage.tintColor = #colorLiteral(red: 1, green: 0.5189241767, blue: 0.3763272166, alpha: 1)
+        locationImage.image = UIImage(named: "loc_image")
         return locationImage
     }()
     
     var locationMultyButton: UIButton = {
         let locationMultyButton = UIButton()
-        locationMultyButton.setImage(UIImage(named: "chevron.down"), for: UIControl.State.normal)
+        locationMultyButton.setBackgroundImage(UIImage(systemName: "chevron.down"), for: .normal)
+        locationMultyButton.tintColor = .lightGray
         return locationMultyButton
+    }()
+    
+    var filterButton: UIButton = {
+        let filterButton = UIButton()
+        filterButton.setBackgroundImage(UIImage(named: "filter_image"), for: .normal)
+        return filterButton
     }()
 
     var selectCategoryView: UIView = {
         let selectCategoryView = UIView()
         selectCategoryView.backgroundColor = .red
         return selectCategoryView
+    }()
+    
+    var selectCategoryLabel: UILabel = {
+        let selectCategoryLabel = UILabel()
+        selectCategoryLabel.textColor = .black
+        selectCategoryLabel.text = "Select Category"
+        selectCategoryLabel.font = UIFont(name: "Mark Pro", size: 25)
+        return selectCategoryLabel
+    }()
+    
+    var selectCategoryButton: UIButton = {
+        let selectCategoryButton = UIButton()
+        selectCategoryButton.tintColor = #colorLiteral(red: 1, green: 0.5189241767, blue: 0.3763272166, alpha: 1)
+        selectCategoryButton.setTitle("view all", for: .normal)
+        return selectCategoryButton
     }()
     
     var categoryColletionView: UICollectionView = {
@@ -85,11 +112,21 @@ class HomeView: UIView {
     }()
     
     func setup() {
+        
+        selectCategoryLabel.setContentCompressionResistancePriority(UILayoutPriority.defaultLow, for: .horizontal)
+//        dateLabel.setContentCompressionResistancePriority(UILayoutPriority.defaultHigh, for: .horizontal)
+        selectCategoryLabel.setContentHuggingPriority(UILayoutPriority.defaultLow, for:.horizontal)
+//        dateLabel.setContentHuggingPriority(UILayoutPriority.defaultHigh, for:.horizontal)
+        
         setupLocationView()
         setupLocationLabel()
         setupLocationImage()
+        setupPointImage()
         setupLocationMultyButton()
-//        setupSelectCategoryView()
+        setupFilterButton()
+        setupSelectCategoryView()
+        setupSelectCategoryLabel()
+        setupSelectCategoryButton()
 //        setupCategoryColletionView()
 //        setupSearchBarView()
 //        setupHotSalesLablesView()
@@ -103,9 +140,8 @@ class HomeView: UIView {
         self.addSubview(locationView)
         locationView.snp.makeConstraints { constraints in
             constraints.top.equalToSuperview().offset(64)
-            constraints.leading.equalToSuperview().offset(5)
-            constraints.trailing.equalToSuperview().offset(-5)
-            constraints.height.equalTo(40)
+            constraints.leading.equalToSuperview()
+            constraints.trailing.equalToSuperview()
         }
     }
     
@@ -121,40 +157,71 @@ class HomeView: UIView {
     private func setupLocationImage() {
         locationView.addSubview(locationImage)
         locationImage.snp.makeConstraints { constraints in
-            constraints.top.equalToSuperview().offset(9)
             constraints.trailing.equalTo(locationLabel.snp.leading).offset(-11)
-            constraints.bottom.equalToSuperview().offset(-9)
-            constraints.width.equalTo(20)
-//            constraints.width.equalTo(locationImage.snp.height)
-//            constraints.centerY.equalToSuperview()
+            constraints.width.equalTo(12)
+            constraints.height.equalTo(15.31)
+            constraints.centerY.equalToSuperview()
+        }
+    }
+    
+    private func setupPointImage() {
+        locationView.addSubview(pointImage)
+        pointImage.snp.makeConstraints { constraints in
+            constraints.trailing.equalTo(locationLabel.snp.leading).offset(-15.12)
+            constraints.width.equalTo(3.77)
+            constraints.height.equalTo(3.77)
+            constraints.top.equalTo(locationImage.snp.top).offset(3.82)
+            constraints.bottom.equalTo(locationImage.snp.bottom).offset(-7.73)
         }
     }
     
     private func setupLocationMultyButton() {
         locationView.addSubview(locationMultyButton)
         locationMultyButton.snp.makeConstraints { constraints in
-            constraints.top.equalToSuperview().offset(5)
-            constraints.trailing.equalToSuperview().offset(-5)
-            constraints.bottom.equalToSuperview().offset(-5)
+            constraints.leading.equalTo(locationLabel.snp.trailing).offset(8)
+            constraints.centerY.equalToSuperview()
+            constraints.width.equalTo(10)
+            constraints.height.equalTo(5)
+        }
+    }
+    
+    private func setupFilterButton() {
+        locationView.addSubview(filterButton)
+        filterButton.snp.makeConstraints { constraints in
+            constraints.trailing.equalToSuperview().offset(-20)
+            constraints.width.equalTo(11)
+            constraints.height.equalTo(13)
+            constraints.centerY.equalToSuperview()
         }
     }
     
     private func setupSelectCategoryView() {
         self.addSubview(selectCategoryView)
         selectCategoryView.snp.makeConstraints { constraints in
-            constraints.top.equalToSuperview().offset(18)
-            constraints.leading.equalToSuperview().offset(5)
-            constraints.trailing.equalToSuperview().offset(-5)
-            constraints.bottom.equalToSuperview().offset(-10)
+            constraints.top.equalTo(locationView.snp.bottom).offset(18)
+            constraints.leading.equalToSuperview()
+            constraints.trailing.equalToSuperview()
+            constraints.height.equalTo(40)
         }
     }
     
-    private func setupCategoryColletionView() {
-        self.addSubview(categoryColletionView)
-        categoryColletionView.snp.makeConstraints { constraints in
-            constraints.top.equalTo(selectCategoryView.snp.bottom).offset(24)
-            constraints.leading.equalToSuperview().offset(5)
-            constraints.trailing.equalToSuperview().offset(-5)
+    private func setupSelectCategoryLabel() {
+        selectCategoryView.addSubview(selectCategoryLabel)
+        selectCategoryLabel.snp.makeConstraints { constraints in
+            constraints.top.equalToSuperview().offset(5)
+            constraints.bottom.equalToSuperview().offset(-5)
+            constraints.center.equalToSuperview()
+            constraints.leading.equalToSuperview().offset(17)
+        }
+    }
+    
+    private func setupSelectCategoryButton() {
+        selectCategoryView.addSubview(selectCategoryButton)
+        selectCategoryButton.snp.makeConstraints { constraints in
+            constraints.top.equalToSuperview().offset(5)
+            constraints.bottom.equalToSuperview().offset(-5)
+            constraints.center.equalToSuperview()
+            constraints.trailing.equalToSuperview().offset(-33)
         }
     }
     
